cb490ff24c6683f6011c545679060f8a0fb73140
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2024-03-06 21:29:57 -0300

    Diagrama Comparando Imprimiendo Pares Vector

---
+++ src/Algoritmos/ImprimiendoPares.java
@@ -0,0 +1,12 @@
+package Algoritmos;
+
+public class ImprimiendoPares {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}

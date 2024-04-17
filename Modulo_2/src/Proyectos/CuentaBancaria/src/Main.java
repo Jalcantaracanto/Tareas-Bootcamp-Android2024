e5b73359a328e7e395b332ca7122901ea1e40c2e
Javier Alcántara Canto <javier.alcantara.canto@gmail.com>
2024-04-17 15:49:08 -0400

    Rama Master se deja vacia

--- Modulo_2/src/Proyectos/CuentaBancaria/src/Main.java
+++
@@ -1,9 +0,0 @@
-import cl.CuentaBancaria.clases.Cliente;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Cliente cliente = Cliente.crearClienteDesdeConsola();
-        cliente.getCuentaCliente().mostrarInformacionCuentaCorriente();
-    }
-}

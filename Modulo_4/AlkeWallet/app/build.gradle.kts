9bec6b6e9b9cb0137d406ffa8540d2df04fc1583
Javier Alcántara Canto <javier.alcantara.canto@gmail.com>
2024-04-26 18:17:36 -0400

    Fragments

--- Modulo_4/AlkeWallet/app/build.gradle.kts
+++ Modulo_4/AlkeWallet/app/build.gradle.kts
@@ -29,14 +29,21 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

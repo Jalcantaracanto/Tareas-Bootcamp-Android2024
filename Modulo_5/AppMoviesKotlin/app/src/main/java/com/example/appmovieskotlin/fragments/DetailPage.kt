d97173e6c64c55816cfe9aef2256a1a1e20fddd4
Jalcantaracanto <javier.alcantara.canto@gmail.com>
2024-05-13 13:58:52 -0400

    Add send email

--- Modulo_5/AppMoviesKotlin/app/src/main/java/com/example/appmovieskotlin/fragments/DetailPage.kt
+++ Modulo_5/AppMoviesKotlin/app/src/main/java/com/example/appmovieskotlin/fragments/DetailPage.kt
@@ -1,11 +1,13 @@
 package com.example.appmovieskotlin.fragments
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -41,8 +43,13 @@ class DetailPage : Fragment() {
         binding.titleMovieDescription.text = movie?.title
         binding.descriptionMovie.text = movie?.overview
 
-        binding.btnVolver.setOnClickListener {
+        binding.btnBack.setOnClickListener {
             requireActivity().onBackPressed()
+        }
+
+
+        binding.btnShare.setOnClickListener {
+            sendEmailMovie(movie!!)
         }
 
         return binding.root
@@ -50,6 +57,27 @@ class DetailPage : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun sendEmailMovie(movie: Movie) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("javier.alcantara.canto@gmail.com"))
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Movie Recommendation")
+
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "I have a movie recommendation that we can watch: ${movie.title} and it has a duration of: ${movie.duration}" +
+                    "The synopsis is \n" +
+                    movie.overview
+        )
+
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(Intent.createChooser(intent, "Send email"))
+        } else {
+            Toast.makeText(requireContext(), "The email could not be sent", Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
 
